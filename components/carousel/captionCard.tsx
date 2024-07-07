5753d46f69889bfcd80ca097dbefc461ab01b526
apoorv jain <apoorvj352@gmail.com>
2024-07-07 14:16:09 +0530

    carousel improvement

--- components/carousel/captionCard.tsx
+++ components/carousel/captionCard.tsx
@@ -19,24 +19,28 @@ export function CaptionCard({
     <CardContainer className="inter-var">
       <CardBody
         className={`bg-cover bg-center bg-no-repeat bg-[url(/assets/mountain.jpg)] bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-[300px] max-md:w-[300px] h-auto rounded-xl p-6 border bg-opacity-20 ${
-          selected ? "border-4 border-black" : ""
+          selected ? "border-4 border-black" : "opacity-40"
         }`}
       >
         <div className="absolute inset-0 w-full h-full bg-black bg-opacity-25 rounded-md"></div>
         <div onClick={togglePopover} className="bg-cover">
           <CardItem translateZ="100" className="w-full mt-4">
             <div className="w-full h-[300px] flex justify-center items-center text-3xl font-bold italic text-white">
-              Sometimes, you just need to change your altitude.
+              {selected
+                ? "Sometimes, you just need to change your altitude."
+                : ""}
             </div>
           </CardItem>
           <div className="flex justify-center items-center mt-6">
-            <CardItem
-              translateZ={20}
-              as="button"
-              className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-            >
-              View Similar
-            </CardItem>
+            {selected && (
+              <CardItem
+                translateZ={20}
+                as="button"
+                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+              >
+                View Similar
+              </CardItem>
+            )}
           </div>
         </div>
       </CardBody>
